a373e7eeddd72958a2280a63e69d71752337abdc
Luis Andes <luisandes@gmail.com>
2019-10-15 17:56:11 +0200

    Primera versión

---
+++ app/src/main/java/com/example/cinesaragon/RegUsrScr.java
@@ -0,0 +1,14 @@
+package com.example.cinesaragon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegUsrScr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_usr_scr);
+    }
+}
