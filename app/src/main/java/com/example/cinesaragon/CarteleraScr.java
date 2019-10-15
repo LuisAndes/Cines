9fa4af62111b6f2215f1d50fa9cfa5856c5d5bc2
Luis Andes <luisandes@gmail.com>
2019-10-15 21:38:38 +0200

    Primera versión

--- app/src/main/java/com/example/cinesaragon/CarteleraScr.java
+++ app/src/main/java/com/example/cinesaragon/CarteleraScr.java
@@ -1,8 +1,8 @@
 package com.example.cinesaragon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CarteleraScr extends AppCompatActivity {
 
