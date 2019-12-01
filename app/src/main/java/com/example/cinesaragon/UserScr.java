9fcd888ef01bc94f9b9ea50ce53c6b9b9548b1fe
Saul Diaz <sefford@gmail.com>
2019-12-01 13:42:37 +0100

    Hooked up Modify Current User with UserRegScreen

--- app/src/main/java/com/example/cinesaragon/UserScr.java
+++ app/src/main/java/com/example/cinesaragon/UserScr.java
@@ -3,30 +3,69 @@ package com.example.cinesaragon;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.cinesaragon.domain.GetCurrentUser;
+import com.example.cinesaragon.domain.ModifyCurrentUser;
 import com.example.cinesaragon.domain.helpers.ResultCallback;
 import com.example.cinesaragon.model.User;
 import com.google.firebase.database.DatabaseError;
 
 import javax.inject.Inject;
 
-public class UserScr extends AppCompatActivity {
+public class UserScr extends BaseActivity {
 
     @Inject
     GetCurrentUser getCurrentUser;
+    @Inject
+    ModifyCurrentUser modifyCurrentUser;
+
+    EditText name;
+    EditText lastName;
+    EditText phone;
+    EditText address;
+    EditText email;
+    EditText card;
+    EditText paypal;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_scr);
-        getCurrentUser.execute(new ResultCallback<User, DatabaseError>() {
+        initializeViews();
+        requestUser();
+    }
+
+    private void initializeViews() {
+        this.name = findViewById(R.id.edtNombre);
+        this.lastName = findViewById(R.id.edtApellidos);
+        this.phone = findViewById(R.id.edtTelefono);
+        this.address = findViewById(R.id.edtDireccion);
+        this.email = findViewById(R.id.edtEmail);
+        this.card = findViewById(R.id.edtNumeroTarjeta);
+        this.paypal = findViewById(R.id.edtPayPal);
+        this.save = findViewById(R.id.btnGuardarPerfil);
+        this.save.setOnClickListener(view -> saveUser());
+    }
+
+    private void setInitialContent(User user) {
+        this.name.setText(user.getName());
+        this.lastName.setText(user.getName());
+        this.phone.setText(user.getPhone());
+        this.address.setText(user.getAddress());
+        this.email.setText(user.getEmail());
+        this.card.setText(user.getCard());
+        this.paypal.setText(user.getPaypal());
+    }
+
+    private void requestUser() {
+        getCurrentUser.retrieve(new ResultCallback<User, DatabaseError>() {
             @Override
             public void onResult(User result) {
-                Log.d("Sefford", "Result:" + result);
+                setInitialContent(result);
             }
 
             @Override
@@ -34,6 +73,31 @@ public class UserScr extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void saveUser() {
+        modifyCurrentUser.modify(this,
+                new User.Builder(this.email.getText().toString())
+                        .setAddress(this.address.getText().toString())
+                        .setCard(this.card.getText().toString())
+                        .setLastName(this.lastName.getText().toString())
+                        .setName(this.name.getText().toString())
+                        .setPaypal(this.paypal.getText().toString())
+                        .setPhone(this.phone.getText().toString())
+                        .build(),
+                new ResultCallback<Void, Exception>() {
+                    @Override
+                    public void onResult(Void result) {
+                        Toast.makeText(UserScr.this, "Modifications saved successfully.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Exception error) {
+                        Toast.makeText(UserScr.this, "Could not save",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public static void goToProfile(Activity activity) {
